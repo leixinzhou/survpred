ec02c33fe90e9afe28704c7e18a735d401a4c240
leixinzhou <llasionzhou@gmail.com>
2019-11-08 15:03:39 -0600

    done 3d dataset

--- dataset/iowa96.py
+++ dataset/iowa96.py
@@ -9,15 +9,14 @@ import random, os
 
 TR_NB = 48
 VAL_NB = 5
-ROOT_DIR = "./Iowa_96_cases_binary_selected/"
+ROOT_DIR = "/home/leizhou/Documents/outlier_detect/survpred/data/iowa_surv/"
 IMG_NAME = "InputCTPrimary_ROI.nii.gz"
 GT_NAME = "GTV_Primary_ROI_CT_ST.nii.gz"
-OUTPUT_DIR = "./IOWA96_np/"
 
 surv_list = []
 dead_list = []
 
-with open('Binary_96_selected.csv') as csvfile:
+with open(os.path.join(ROOT_DIR, 'Binary_96_selected.csv')) as csvfile:
     reader  = csv.reader(csvfile)
     next(reader)
     for row in reader:
@@ -25,8 +24,8 @@ with open('Binary_96_selected.csv') as csvfile:
             surv_list.append(row[0])
         else:
             dead_list.append(row[0])
-print("Surv case # :", len(surv_list))
-print("Dead case # :", len(dead_list))
+# print("Surv case # :", len(surv_list))
+# print("Dead case # :", len(dead_list))
 
 random.seed(0)
 random.shuffle(surv_list)
@@ -39,45 +38,45 @@ class IOWA96(Dataset):
     """
     Model the IOWA96 dataset.
     """
-    def __init__(self, img_dir, seg_gt_dir):
+    def __init__(self, train=True):
         """
         Class constructor.
 
-        :param img_dir: the img numpy file
-        :param seg_gt_dir: the segmentation gt numpy file
+        :param train: training dataset or not
         """
         super(IOWA96, self).__init__()
 
-        self.img_dir = img_dir
-        self.seg_gt_dir = seg_gt_dir
-
-        self.img = np.load(self.img_dir)
-        self.seg_gt = np.load(self.seg_gt_dir)
+        self.train = train
+        if self.train:
+            self.case_list = tr_case
+        else:
+            self.case_list = val_case
 
     def __len__(self):
-        return self.img.shape[0]
+        return len(self.case_list)
 
     def __getitem__(self, i):
-        tmp_img = np.clip(self.img[i,], -500, 200)
-        tmp_img -= np.mean(tmp_img)
-        tmp_img /= np.std(tmp_img)
-        tmp_gt = self.seg_gt[i,]
-        tmp_img = torch.from_numpy(tmp_img.astype(np.float32)).unsqueeze(0)
-        tmp_gt = torch.from_numpy(tmp_gt.astype(np.float32)).unsqueeze(0)
+        img = os.path.join(ROOT_DIR, 'Iowa_96_cases_binary_selected', self.case_list[i], IMG_NAME)
+        gt = os.path.join(ROOT_DIR, 'Iowa_96_cases_binary_selected', self.case_list[i], GT_NAME)
+        img = sitk.GetArrayFromImage(sitk.ReadImage(img))
+        gt = sitk.GetArrayFromImage(sitk.ReadImage(gt))
+        img = np.clip(img, -500, 200)
+        img -= np.mean(img)
+        img /= np.std(img)
+        img = torch.from_numpy(img.astype(np.float32)).unsqueeze(0)
+        gt = torch.from_numpy(gt.astype(np.float32)).unsqueeze(0)
 
-        return tmp_img, tmp_gt
+        return img, gt
 
 
 if __name__ == "__main__":
-    img_dir = "data/iowa_surv/IOWA96_np/tr_img.npy"
-    gt_dir = "data/iowa_surv/IOWA96_np/tr_seg_gt.npy"
-    dataset = IOWA96(img_dir, gt_dir)
-    loader = DataLoader(dataset, shuffle=True)
+    dataset = IOWA96()
+    loader = DataLoader(dataset, shuffle=False)
     for _, (img, gt) in enumerate(loader):
         fig, axes = plt.subplots(1,2)
-        pos = axes[0].imshow(img.squeeze(), cmap='gray')
+        pos = axes[0].imshow(img.squeeze()[0,], cmap='gray')
         fig.colorbar(pos, ax=axes[0])
-        pos = axes[1].imshow(gt.squeeze())
+        pos = axes[1].imshow(gt.squeeze()[0,])
         fig.colorbar(pos, ax=axes[1])
         plt.show()
         break
